81b7ec44a697acc0a37446ef5df0e77cd1e4fb4a
helena furtado <helenafurtado25@gmail.com>
2026-03-14 00:39:27 -0300

    começo do projeto

---
+++ main.py
@@ -0,0 +1,21 @@
+import pygame 
+pygame.init()
+
+
+screen = pygame.display.set_mode((800, 400)) # criando display surfice 
+pygame.display.set_caption("jogo do corredooooorr") # criando o nome do display surfice
+clock = pygame.time.Clock()
+
+test_surface = pygame.Surface((100, 200)) # criando uma superfici teste para treinar coordenas em BLIT 
+test_surface.fill('Red')
+running = True
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+            
+
+    #screen.blit(test_surface, (200,100))
+    
+    pygame.display.update()
+    clock.tick(60)
