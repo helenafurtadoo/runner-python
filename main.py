b11849314e404f7ff2cafaaa0e879ce138d5b692
helena furtado <helenafurtado25@gmail.com>
2026-03-14 23:36:35 -0300

    docs: add coloros and imporve the desing

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ pygame.init()
 
 # DISPLAY SURFACE
 screen = pygame.display.set_mode((800, 400)) # criando display surfice (janela de exibição)
-pygame.display.set_caption("jogo do corredooooorr") # criando o nome do display surfice
+pygame.display.set_caption("Runner game") # criando o nome do display surfice
 clock = pygame.time.Clock()
 
 # FONTE
@@ -14,8 +14,8 @@ sky_surface = pygame.image.load('grafico/ceu.png').convert() #.convert() otimiza
 ground_surface = pygame.image.load('grafico/chao.png').convert()
 
 # SCORE
-score_surface = test_font.render('Jogo do corredorrrrr', False, 'Purple') #AA desativado
-socre_rectangle = score_surface.get_rect(center = (400, 50))
+score_surface = test_font.render('Runner game', False,(64,64,64)) #AA desativado
+score_rectangle = score_surface.get_rect(center = (400, 50))
 
 # SNAIL SURFACE
 snail_surface = pygame.image.load('grafico\snail\snail1.png').convert_alpha()  
@@ -24,6 +24,7 @@ snail_rectangle = snail_surface.get_rect(bottomright = (600, 300))
 # PLAYER SURFACE
 player_surface = pygame.image.load('grafico\player\player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom = (80, 300))
+
 
 
 running = True
@@ -41,7 +42,8 @@ while running:
     # PRINTING THE SURFACES
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(score_surface, socre_rectangle)
+    pygame.draw.rect(screen, '#c0e8ec', score_rectangle,border_radius = 10)
+    screen.blit(score_surface, score_rectangle)
 
     snail_rectangle.x-= 4
     if snail_rectangle.right <= 0: snail_rectangle.left = 800 # se a parte direita retangulo da lesma for <= 0 (), "renascer" a lesma no 800 (mais esquerda possivel do x)
