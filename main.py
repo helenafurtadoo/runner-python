d7825654801e96191e71879b7c13a136ff4dc640
helena furtado <helenafurtado25@gmail.com>
2026-03-14 21:39:55 -0300

    add player1 + rectangle

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ pygame.display.set_caption("jogo do corredooooorr") # criando o nome do display 
 clock = pygame.time.Clock()
 test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
-sky_surface = pygame.image.load('grafico/ceu.png') 
-ground_surface = pygame.image.load('grafico/chao.png')
-text_surface = test_font.render('testando o texto', False, 'Black') #AA desativado
+sky_surface = pygame.image.load('grafico/ceu.png').convert() #.convert() otimiza o desempenho da img
+ground_surface = pygame.image.load('grafico/chao.png').convert()
+text_surface = test_font.render('Jogo do corredorrrrr', False, 'Purple') #AA desativado
 
-snail_surface = pygame.image.load('grafico\snail\snail1.png')
+snail_surface = pygame.image.load('grafico\snail\snail1.png').convert_alpha()  
 snail_pos_x = 600
 
+player_surface = pygame.image.load('grafico\player\player_walk_1.png').convert_alpha()
+player_rectangle = player_surface.get_rect(midbottom = (80, 300))
 running = True
 while running:
     for event in pygame.event.get():
@@ -23,10 +25,11 @@ while running:
 
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
-    screen.blit(text_surface, (300,50))
+    screen.blit(text_surface, (200,50))
     snail_pos_x -= 4 # para andar para a esquerda + velocidade
     if snail_pos_x < -100: snail_pos_x = 800
     screen.blit(snail_surface, (snail_pos_x, 250))
+    screen.blit(player_surface, player_rectangle)
    
     
     pygame.display.update()
