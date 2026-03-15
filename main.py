fec7ff08cea1c544f125f407448535544611ede5
helena furtado <helenafurtado25@gmail.com>
2026-03-14 21:50:01 -0300

    docs: organizing the comments

--- main.py
+++ main.py
@@ -1,28 +1,37 @@
 import pygame 
 pygame.init()
 
-
-screen = pygame.display.set_mode((800, 400)) # criando display surfice 
+# DISPLAY SURFACE
+screen = pygame.display.set_mode((800, 400)) # criando display surfice (janela de exibição)
 pygame.display.set_caption("jogo do corredooooorr") # criando o nome do display surfice
 clock = pygame.time.Clock()
-test_font = pygame.font.Font('font/Pixeltype.ttf', 50)
 
+# FONTE
+test_font = pygame.font.Font('font/Pixeltype.ttf', 50) 
+
+# REGULAR SURFICES
 sky_surface = pygame.image.load('grafico/ceu.png').convert() #.convert() otimiza o desempenho da img
 ground_surface = pygame.image.load('grafico/chao.png').convert()
 text_surface = test_font.render('Jogo do corredorrrrr', False, 'Purple') #AA desativado
 
+# SNAIL SURFACE
 snail_surface = pygame.image.load('grafico\snail\snail1.png').convert_alpha()  
 snail_pos_x = 600
 
+
+# PLAYER SURFACE
 player_surface = pygame.image.load('grafico\player\player_walk_1.png').convert_alpha()
 player_rectangle = player_surface.get_rect(midbottom = (80, 300))
+
+
 running = True
 while running:
     for event in pygame.event.get():
+        # CHECANDO O DISPLAY SURFICE PARA PODER FECHAR 
         if event.type == pygame.QUIT:
             running = False
             
-
+    # PRINTING THE SURFACES
     screen.blit(sky_surface, (0,0))
     screen.blit(ground_surface, (0, 300))
     screen.blit(text_surface, (200,50))
@@ -32,5 +41,6 @@ while running:
     screen.blit(player_surface, player_rectangle)
    
     
+    # ATUALIZANDO TUDO
     pygame.display.update()
     clock.tick(60)
